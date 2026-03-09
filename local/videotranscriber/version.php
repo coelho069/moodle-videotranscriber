cae435284c2e571c396da6b24c81e7f645d7ca97
mateus <secmateuss@gmail.como>
2026-03-09 13:08:49 -0400

    Fix: Update module version to 2032010100 to force upgrade on VM

--- local/videotranscriber/version.php
+++ local/videotranscriber/version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2030010100;
+$plugin->version   = 2032010100;
 $plugin->requires  = 2022041900;
 $plugin->component = 'local_videotranscriber';
 $plugin->maturity  = MATURITY_STABLE;
--- mod/url/version.php
+++ mod/url/version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2030010100;
+$plugin->version   = 2032010100;
 $plugin->requires  = 2024100100;
 $plugin->component = 'mod_url';
 $plugin->cron      = 0;
