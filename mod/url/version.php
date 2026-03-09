e0442ebac43a4bfb5ab3a3ef2e1df32523fc44f4
mateus <secmateuss@gmail.como>
2026-03-09 12:09:54 -0400

    Fix: Versão estável com Tutor IA e ajustes de versão

--- mod/url/version.php
+++ mod/url/version.php
@@ -9,11 +9,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-<<<<<<< HEAD
 $plugin->version   = 2026030910;       // The current module version (Date: YYYYMMDDXX).
-=======
-$plugin->version   = 2026030902;       // The current module version (Date: YYYYMMDDXX).
->>>>>>> adcda93158a6a0a8176cce1878a0c82b44c11986
 $plugin->requires  = 2024100100;    // Requires this Moodle version.
 $plugin->component = 'mod_url';        // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
